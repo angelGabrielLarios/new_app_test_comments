ad54df98b9adb50e576a6a7bc7e147f24e4c5497
Angel Larios <lariosacostaa@gmail.com>
2023-12-07 13:57:00 -0600

    se limpio el string

--- src/chatgpt3/isCommentOffensive.js
+++ src/chatgpt3/isCommentOffensive.js
@@ -1,11 +1,13 @@
 import { openAI } from "./openAI.js";
 
 export async function isCommentOffensive(comment = 'hola') {
+
+
     try {
         const completion = await openAI.chat.completions.create({
 
 
-            messages: [{ role: "system", content: `Te proporcionaré un comentario en español proveniente de México. Tu tarea consistirá en analizar el contexto del comentario para identificar las siguientes características: si contiene expresiones de odio hacia una persona o grupo, si discrimina a alguien o a un grupo de personas, si es obsceno, si implica acoso sexual, si tiene la intención de dañar a una persona o grupo, y si ofende la orientación sexual de alguien o un grupo de personas. Finalmente, determina si incluye groserías ofensivas hacia alguien o un grupo de personas. Si el comentario cumple con alguna de estas características, devuelve el número 1; si es un comentario neutro que no cumple con ninguna de las características mencionadas, devuelve un 0. Simplemente devuelve el número sin proporcionar ninguna explicación de la razón por la que lo seleccionaste.El comentario es "${comment}"` }],
+            messages: [{ role: "system", content: `Te proporcionaré un comentario en español proveniente de México. Tu tarea consistirá en analizar el contexto del comentario para identificar las siguientes características: si contiene expresiones de odio hacia una persona o grupo, si discrimina a alguien o a un grupo de personas, si es obsceno, si implica acoso sexual, si tiene la intención de dañar a una persona o grupo, y si ofende la orientación sexual de alguien o un grupo de personas. Finalmente, determina si incluye groserías ofensivas hacia alguien o un grupo de personas. Si el comentario cumple con alguna de estas características, devuelve el número 1; si es un comentario neutro que no cumple con ninguna de las características mencionadas, devuelve un 0. Simplemente devuelve el número sin proporcionar ninguna explicación de la razón por la que lo seleccionaste.El comentario es "${comment.trim().toLocaleLowerCase()}"` }],
             model: "gpt-3.5-turbo",
         })
 
